629d2c4747d4d1e178fb0752f7737c2909fd754d
Rafael Adão <raphinha.oliveira11@gmail.com>
2023-02-14 02:59:18 -0300

    feat: change mail html table

--- send_mail.php
+++ send_mail.php
@@ -14,13 +14,15 @@ $message    = "
         <body>
             <table>
                 <tr>
-                    <th>Nome</th>
-                    <th>E-mail</th>
-                    <th>Mensagem</th>
+                    <th align='right'>Nome:</th>
+                    <td>$name</td>
                 </tr>
                 <tr>
-                    <td>$name</td>
+                    <th align='right'>E-mail:</th>
                     <td>$email</td>
+                </tr>
+                <tr>
+                    <th align='right'>Mensagem:</th>
                     <td>$message</td>
                 </tr>
             </table>
