09d3314c696065464216cf2a26ef6170b7a73588
Rafael Adão <raphinha.oliveira11@gmail.com>
2023-02-14 02:41:39 -0300

    feat: add changes to send mail script

--- send_mail.php
+++ send_mail.php
@@ -1,13 +1,37 @@
 <?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+$name       = $_POST['name'];
+$email      = $_POST['email'];
+$message    = $_POST['message'];
 
-$from = "rafaeldeoliveiraadao@gmail.com";
-$to = "rafaeldeoliveiraadao@gmail.com";
-$subject = "Checking PHP mail";
-$message = "PHP mail works just fine";
-$headers = "From:" . $from;
+$to         = "rafaeldeoliveiraadao@gmail.com";
+$subject    = "Novo e-mail! (rafinhaadao.com)";
 
-if (mail($to, $subject, $message, $headers)) {
-    echo "E-mail enviado com sucesso!";
-}
+$message    = "
+    <html>
+        <head>
+            <title>Novo e-mail! (rafinhaadao.com)</title>
+        </head>
+        <body>
+            <table>
+                <tr>
+                    <th>Nome</th>
+                    <th>E-mail</th>
+                    <th>Mensagem</th>
+                </tr>
+                <tr>
+                    <td>$name</td>
+                    <td>$email</td>
+                    <td>$message</td>
+                </tr>
+            </table>
+        </body>
+    </html>
+";
+
+$headers    = "MIME-Version: 1.0" . "\r\n";
+$headers    .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$headers    .= "From: $email" . "\r\n";
+$headers    .= "Cc: raphinha.oliveira11@gmail.com" . "\r\n";
+
+mail($to, $subject, $message, $headers);
+header("Location: ./");
